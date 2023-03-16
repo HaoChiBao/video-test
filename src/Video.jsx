85b6126e537d9d854d9485ddd45e9a07de6917c9
ethan-rng <ethan.rong@gmail.com>
2023-03-16 01:22:27 -0400

    nothing much lmao

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { VideoRoom } from './components/VideoRoom';
-import { IconBar } from './components/IconBar'
+import React from "react";
+import { VideoRoom } from "./components/VideoRoom";
+import { IconBar } from "./components/IconBar";
+
 
 export default function Video() {
-    return (
+    let isMediaPipeOn = false;
 
+    const turnMediaPipeOn = () => {
+        return (<p>fadsfadf</p>)
+    }
+    return (
         <div>
             <h1>Meeting Name</h1>
+            <button onClick={turnMediaPipeOn()}>Turn On Hand Tracking</button>
             <VideoRoom></VideoRoom>
             <IconBar></IconBar>
         </div>
-    )
+  );
 }
